f275e05e8e607e0628f7387e76c124fc2abe6dd0
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-08-01 03:30:22 -0400

    Editando estilos del navbar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
   <div className="text-center mt-5">
     <h1>Hello Rigo!</h1>
     <h3>Hola mundo!</h3>
-    <p>
-      <img src={rigoImage} />
-    </p>
     <a href="#" className="btn btn-success">
       If you see this green button, bootstrap is working
     </a>
