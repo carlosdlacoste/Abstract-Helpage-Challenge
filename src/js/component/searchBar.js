c36e88b71b697b6f1f3f3ab3b779c6b15f25cb6c
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-08-18 20:07:39 -0400

    Ubicado el arrow icon al final de la barra de busqueda

--- src/js/component/searchBar.js
+++ src/js/component/searchBar.js
@@ -5,7 +5,10 @@ export const SearchBar = () =>{
         <>
             <div className="flex place-content-center items-center bg-indigo-200 h-72 flex-col">
                 <h1 className="text-7xl font-bold">How Can We Help?</h1>
-                <input className="mt-6 w-2/4 h-12 p-3 rounded" placeholder="Search"></input>
+                <div className="mt-6 w-2/4 h-12 relative">
+                    <input className="w-full p-3 rounded absolute" placeholder="Search"></input>
+                    <i className="fa-solid fa-arrow-right fa-lg absolute right-3 top-1/2 -translate-y-2/4 "></i>
+                </div>
             </div>
         </>
     );
