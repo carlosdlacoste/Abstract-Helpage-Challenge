26b431f8fa0df24b128339e8efa0b4d8059b3049
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-08-19 03:46:36 -0400

    Creado nuevo componente para la seccion de categorias en home

---
+++ src/js/component/homeItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const HomeItems= () =>{
+    return(
+        <>
+            <div>
+                <div className="grid grid-cols-2 gap-x-4 gap-y-8 p-3">
+                    <div>
+                        <h5>Branches</h5>
+                        <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../../styles/home.css";
 import { SearchBar } from "../component/searchBar";
+import { HomeItems } from "../component/homeItems";
 
 export const Home = () => (
   <>
     <SearchBar/>
+    <HomeItems/>
   
   </>
 );
